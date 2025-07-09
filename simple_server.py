e4c684f95775a124b31b90bbcb8cc0cddf9de9d4
feliramosz <feliperamosz@gmail.com>
2025-07-08 20:49:17 -0400

    Refactoring

--- simple_server.py
+++ simple_server.py
@@ -27,7 +27,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 HOST_NAME = '0.0.0.0'
 PORT_NUMBER = 8000
 load_dotenv(dotenv_path=os.path.join(os.path.expanduser('~'), 'senapred-monitor.env'))
