d87e5e4200b901b91432adbfab6b6852ac202e1f
feliramosz <feliperamosz@gmail.com>
2025-07-19 06:17:05 -0400

    Corrección a script para datos DGA

--- test_dga.py
+++ test_dga.py
@@ -10,9 +10,7 @@ def configurar_driver():
     """Configura el navegador Chrome para ejecutarse en segundo plano en el servidor."""
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
+    # ... (otras opciones) ...
     driver = webdriver.Chrome(options=options)
     return driver
 
