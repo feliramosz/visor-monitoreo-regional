dafa501881138b9ae15ca0cc661b46c0cf86dec0
feliramosz <feliperamosz@gmail.com>
2025-07-19 06:23:07 -0400

    Ajuste en script para DGA

--- test_dga.py
+++ test_dga.py
@@ -5,18 +5,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 
 def configurar_driver():
     """Configura el navegador Chrome para ejecutarse en segundo plano en el servidor."""
     options = webdriver.ChromeOptions()
-    # --- LÍNEA AÑADIDA: Le decimos a Selenium dónde está Chrome ---
     options.binary_location = "/usr/bin/google-chrome-stable"
 
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36")
-    driver = webdriver.Chrome(options=options)
+
+    # --- LÍNEA AÑADIDA: Le decimos a Selenium dónde está el "controlador" ---
+    service = Service(executable_path="/usr/bin/chromedriver")
+
+    driver = webdriver.Chrome(service=service, options=options)
     return driver
 
 def obtener_datos_dga_selenium():
